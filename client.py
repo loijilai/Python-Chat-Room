29efdd8432075c75f229092f16e7b7c112ba8e48
loijilai <loijilai@gmail.com>
2025-09-26 16:50:57 +0800

    Add register and login logic

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import threading
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,9 +8,37 @@ load_dotenv()
 host = os.getenv('HOST')
 port = int(os.getenv('PORT'))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
-    s.sendall(b'hello, world')
-    data = s.recv(1024)
+def recv_from_server(s):
+    while True:
+        try:
+            data = s.recv(1024).decode()
+            if not data:
+                print("Server closed connection.")
+                break
+            print(f"[Server] {data}")
+        except OSError as e:
+            print(f"Unexpected error: {e}")
+            break
 
-print(f'recv {data}!')
+def connect_to_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    print('Connected to server!')
+
+    receive_thread = threading.Thread(target=recv_from_server, args=(s,), daemon=True)
+    receive_thread.start()
+
+    while True:
+        try:
+            text = input('')
+            s.sendall(text.encode())
+            if text == 'logout':
+                break
+
+        except OSError as e:
+            print(f"Unexpected error: {e}")
+            break
+
+    s.close()
+
+connect_to_server()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import threading
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,14 +8,61 @@ load_dotenv()
 host = os.getenv('HOST')
 port = int(os.getenv('PORT'))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+users = {}
+online_users = {}
+
+def handle_client(conn):
+    try:
+        while True:
+            conn.sendall(b"Type 'login' or 'register' to enter the chatroom")
+            data = conn.recv(1024).decode()
+            if data.lower() == 'register':
+                conn.sendall(b'Enter your username:')
+                username = conn.recv(1024).decode()
+                if username in users:
+                    conn.sendall(b'Username already in use, try another one')
+                    continue
+
+                conn.sendall(b'Enter your password:')
+                password = conn.recv(1024).decode()
+                users[username] = {'password': password, 'room_id': 0}
+                conn.sendall(b'Register successfully! You can now login\n')
+
+            elif data.lower() == 'login':
+                conn.sendall(b'Enter your username:')
+                username = conn.recv(1024).decode()
+                if username not in users:
+                    conn.sendall(b'Username not exists, try login first')
+                    continue
+                conn.sendall(b'Enter your password:')
+                password = conn.recv(1024).decode()
+                if password != users[username]['password']:
+                    conn.sendall(b'Wrong password!')
+                    continue
+                conn.sendall(b'Login successfully! You can now chat!')
+                break
+
+        while True:
+            # start chatting
+            conn.sendall(b"Type 'list', 'enter <room id>', 'create', 'exit' to start using the chatroom")
+            data = conn.recv(1024).decode()
+            print(f'client says: {data}')
+    except OSError as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        # TODO: final cleanup
+        conn.close()
+
+def accept_new_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    print(f'Server start listening on ({host}, {port})')
+    while True:
+        conn, addr = s.accept()
+        print(f'Connected by {addr}')
+        thread = threading.Thread(target=handle_client, args=(conn,))
+        thread.start()
+
+accept_new_connection()
